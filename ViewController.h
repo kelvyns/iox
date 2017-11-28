15e09af629dc2ff668b9e5cefd90528ed67cb8d9
EducacionIT <educacionit@iMac-de-EducacionIT.local>
2017-11-27 21:15:20 -0300

    firts files

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrimeraAplicacion
+//
+//  Created by EducacionIT on 27/11/17.
+//  Copyright © 2017 EducacionIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PrimeraAplicacion
+//
+//  Created by EducacionIT on 27/11/17.
+//  Copyright © 2017 EducacionIT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
